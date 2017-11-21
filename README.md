e2f6516d7ee0150fecded48f3613944d1042fabc
Ryan Gill <ryanjgill2@gmail.com>
2017-11-21 15:31:42 -0600

    stubbing routes

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # crypto_miner_monitor
 Monitor temperatures of cryptocurrency mining hardware.
+
+## Alexa integration
+- "Alexa, ask Crypto Mining Monitor for tempertaure of the case/GPUs."
+- "Alexa, ask Crypto Mining Monitor to restart the mining rig."
+
+
+## Stack
+- Express webserver running on linux device (Pi, Linkit 7688, Khadas Vim) 
+  - GET routes for temperatures
+  - POST route for reseting rig PSU
+  - Johnny-five to communicate to MRK1000
+    - Temperature probes per GPU and case
+    - Relay for PSU reset
+
+- MKR1000 
+  - running firmata and communicating to linux webserver with built in wifi module through firmata/johnny-five
+  - temperature probes connected to analog inputs
---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const bodyParser = require('body-parser')
+const PORT = 3000
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// view engine setup
+app.set('views', path.join(__dirname, '/views'));
+app.use('/public/css', express.static(__dirname + '/public/css'));
+app.use('/public/js', express.static(__dirname + '/public/js'));
+app.set('view engine', 'pug');
+
+app.get('/', (req, res, next) => res.render('index'))
+
+app.get('/temperature/case', (req, res, next) => {
+  console.log('User requested temp for case.');
+
+  let result = {
+    temperature: Math.round(Math.random() * 100)
+  }
+
+  res.json(result)
+})
+
+app.get('/temperature/gpus', (req, res, next) => {
+  console.log('User requested temp for GPUs.')
+
+  let result = {
+    temperature: Math.round(Math.random() * 100)
+  }
+
+  res.json(result)
+})
+
+app.post('/reset', (req, res, next) => {
+  console.log(req.body);
+  res.sendStatus(200)
+})
+
+app.use((req, res, next) => {
+  console.log(`${req.url} --> 404`)
+  res.sendStatus(404)
+})
+
+app.listen(PORT, () => console.log(`Up and running on port ${PORT}.`))
