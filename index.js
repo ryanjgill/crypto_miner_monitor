8fb79928500bdb3720a9db1f1a5c595f20fc8167
Ryan Gill <ryanjgill2@gmail.com>
2017-11-22 12:52:11 -0600

    mkr1000 connection

--- index.js
+++ index.js
@@ -15,6 +15,125 @@ app.use('/public/css', express.static(__dirname + '/public/css'));
 app.use('/public/js', express.static(__dirname + '/public/js'));
 app.set('view engine', 'pug');
 
+
+// MKR1000 stuffs
+let httpServer = require('http').Server(app)
+let io = require('socket.io')(httpServer)
+let net = require('net')
+let five = require('johnny-five')
+let firmata = require('firmata')
+
+// set options to match Firmata config for wifi
+// using MKR1000 with WiFi101
+const options = {
+  host: CONFIG.MKR1000_IP,
+  port: CONFIG.MKR1000_PORT
+}
+
+// connection starts here
+net.connect(options, function() { //'connect' listener
+  console.log('connected to server!')
+
+  let socketClient = this
+
+  // use the socketClient instead of a serial port for transport
+  let boardIo = new firmata.Board(socketClient)
+
+  boardIo.once('ready', function(){
+    console.log('boardIo ready')
+
+    boardIo.isReady = true
+
+    let board = new five.Board({io: boardIo, repl: true})
+
+    board.on('ready', function() {
+      // full Johnny-Five support here
+      console.log('five ready')
+
+      // setup led on pin 6 --> led pin for MKR1000
+      led = new five.Led(6)
+
+      // pulse led to indicate the board is communicating
+      pulseLed(led, 2000, function () {
+        console.log('LED √')
+      })
+
+      // setup temperature sensor LM35
+      tempSensor = new five.Thermometer({
+        controller: 'LM35',
+        pin: 'A1',
+        freq: 250
+      })
+
+      // setup moisture sensor to correct pin
+      moistureSensor = new five.Sensor({
+        pin: 'A2',
+        freq: 250
+      })
+
+      // setup light sensor to correct pin
+      lightSensor = new five.Sensor({
+        pin: 'A3',
+        freq: 250
+      })
+
+      io.on('connection', function (socket) {
+        console.log(socket.id)
+
+        // emit usersCount on new connection
+        emitUsersCount(io)
+
+        // emit usersCount when connection is closed
+        socket.on('disconnect', function () {
+          emitUsersCount(io)
+        })
+      })
+
+      // emit chart data on each interval
+      // setInterval(function () {
+      //   emitChartData(io, tempSensor, lightSensor, moistureSensor)
+      // }, 1000)
+
+    })
+  })
+
+}).on('error', function (err) {
+  console.log('Unable to connect!')
+  console.log('Please make sure you have the latest StandardFirmataWifi sketch loaded on the MKR1000')
+})
+
+// emit usersCount to all sockets
+function emitUsersCount(io) {
+  io.sockets.emit('usersCount', {
+    totalUsers: io.engine.clientsCount
+  })
+}
+
+// emit chart data to all sockets
+function emitChartData(io, tempSensor, lightSensor, moistureSensor) {
+  io.sockets.emit('chart:data', {
+    date: new Date().getTime(),
+    value: [getTemp(tempSensor), getLight(lightSensor), getMoisture(moistureSensor)]
+  })
+}
+
+// get temperature measurement
+function getTemp(tempSensor) {
+  return Math.round(tempSensor.fahrenheit - 25)
+}
+
+// get light measurement
+function getLight(lightSensor) {
+  return Math.round(lightSensor.value/1023*100)
+}
+
+// get moisture measurement
+function getMoisture(moisture) {
+  return Math.round(moisture.value/1023*100)
+}
+
+
+// Routes
 app.get('/', (req, res, next) => res.render('index'))
 
 app.get('/temperature/case', (req, res, next) => {
@@ -52,4 +171,5 @@ app.use((req, res, next) => {
   res.sendStatus(404)
 })
 
+//httpServer.listen(PORT)
 app.listen(PORT, () => console.log(`Up and running at ${ADDRESS}:${PORT}`))
